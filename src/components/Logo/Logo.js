cd3f4c9431bbf03919e42e427c6e7cc6ada472c3
Stefan Nordbruch <nordi.login@gmail.com>
2019-04-14 16:17:55 +1200

    Logo update

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 
 import config from '../../config';
 import IconLogo from './IconLogo';
-import LogoImage from './saunatime-logo.png';
+import LogoImage from './changemarks_logo.png';
 import css from './Logo.css';
 
 const Logo = props => {
